48fc22f922ebf19a4c04756f138573a588d2c2d6
myth929 <422896780@qq.com>
2022-08-17 20:34:09 +0800

    主题搜索高亮颜色修改

--- lua/config/plugins/nv_github-nvim-theme.lua
+++ lua/config/plugins/nv_github-nvim-theme.lua
@@ -13,19 +13,8 @@ function M.load()
         theme_style = "dark",
         function_style = "italic",
         sidebars = {"qf", "vista_kind", "terminal", "packer"},
-
-        -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-        colors = {hint = "orange", error = "#ff0000"},
-
-        -- Overwrite the highlight groups
-        overrides = function(c)
-            return {
-                htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-                DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-                -- this will remove the highlight groups
-                TSField = {},
-            }
-        end
+        -- error显示『#ff0000』,搜索显示『#613214』
+        colors = {error = "#ff0000", bg_search="#613214"},
     })
 end
 
