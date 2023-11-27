1dbf17ae9aee7cd20b73530e398c2ba5166098ca
myth929 <422896780@qq.com>
2023-11-27 14:18:47 +0800

    change autopairs config

--- lua/pluginsConfig/plugins/auto-completion.lua
+++ lua/pluginsConfig/plugins/auto-completion.lua
@@ -7,7 +7,10 @@ return {
             {
                 "windwp/nvim-autopairs",
                 config = function()
-                    require("nvim-autopairs").setup()
+                    require("nvim-autopairs").setup {
+                        fast_wrap = {},
+                        disable_filetype = { "TelescopePrompt", "vim" },
+                    }
 
                     -- setup cmp for autopairs
                     local cmp_autopairs = require "nvim-autopairs.completion.cmp"
@@ -19,28 +22,28 @@ return {
                 "hrsh7th/cmp-buffer",
                 "hrsh7th/cmp-path",
                 "saadparwaiz1/cmp_luasnip",
-            }
+            },
         },
         config = function()
-            local cmp = require 'cmp'
-            require('cmp').setup {
+            local cmp = require "cmp"
+            require("cmp").setup {
                 preselect = cmp.PreselectMode.None,
                 snippet = { -- snippet 插件设置
                     expand = function(args)
-                        require('snippet').lsp_expand(args.body)
+                        require("snippet").lsp_expand(args.body)
                     end,
                 },
-                mapping = cmp.mapping.preset.insert({ -- 快捷键
-                    ['<Tab>'] = cmp.mapping.select_next_item(),
-                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-                    ['<CR>'] = cmp.mapping.confirm(),
-                }),
+                mapping = cmp.mapping.preset.insert { -- 快捷键
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"] = cmp.mapping.confirm(),
+                },
                 sources = { -- 补全优先级. LSP 优先级最高, 其次是 buffer 和 path
-                    { name = 'nvim_lsp' },
-                    { name = 'buffer' },
-                    { name = 'path' },
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
                 },
             }
-        end
-    }
+        end,
+    },
 }
