8324259032d984db1132ce0c4bd19b5afedd1cdc
gaozhengguo <gaozhengguo@MacBook-Pro.local>
2024-07-02 10:46:05 +0800

    git blame inline

--- lua/pluginsConfig/plugins/gitsigns.lua
+++ lua/pluginsConfig/plugins/gitsigns.lua
@@ -2,7 +2,9 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require('gitsigns').setup()
+            require('gitsigns').setup({
+                current_line_blame = true
+            })
         end
     }
 }
