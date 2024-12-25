bd2060114d12dc07d1b3422c6847f0d0b54ea31d
gaozhengguo <gaozhengguo@MacBook-Pro.local>
2024-12-25 16:43:02 +0800

    upgrade neovim(0.10.3) && update dependencies

--- lua/pluginsConfig/plugins/buffer.lua
+++ lua/pluginsConfig/plugins/buffer.lua
@@ -11,7 +11,6 @@ return {
                 options = {
                     style_preset = bufferline.style_preset.minimal, -- 设置标签栏的样式预设 bufferline.style_preset.default / bufferline.style_preset.minimal
                     diagnostics = "nvim_lsp",                       -- 显示lsp诊断信息
-                    diagnostics_update_in_insert = true,            -- 插入模式中更新诊断信息
                 }
             }
         end
